98671ae8b9de87175729b9f4b27938dfddb59331
Allan Ritchie <allan.ritchie@gmail.com>
2021-12-20 01:01:27 -0500

    Update LocalizationExtensions.cs

--- src/Shiny.Extensions.Localization/LocalizationExtensions.cs
+++ src/Shiny.Extensions.Localization/LocalizationExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using Shiny.Extensions.Localization;
 
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Reflection;
 
@@ -9,6 +11,35 @@ namespace Shiny
 {
     public static class LocalizationExtensions
     {
+        /// <summary>
+        /// This is a useful method if you want to serialize all sections for a specified culture to something like JSON
+        /// </summary>
+        /// <param name="manager"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public static IReadOnlyDictionary<string, IReadOnlyDictionary<string, string>> GetAllSectionsWithKeys(this ILocalizationManager manager, CultureInfo culture)
+        {
+            var dict = new Dictionary<string, IReadOnlyDictionary<string, string>>();
+            foreach (var section in manager.Sections)
+            {
+                var values = section.GetValues(culture);
+                dict.Add(section.Name, values);
+            }
+            return dict;
+        }
+
+
+        /// <summary>
+        /// Quick shortcut for getting a sections key/values for a specified culture
+        /// </summary>
+        /// <param name="manager"></param>
+        /// <param name="sectionName"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public static IReadOnlyDictionary<string, string> GetSectionValues(this ILocalizationManager manager, string sectionName, CultureInfo culture)
+            => manager.GetSection(sectionName)!.GetValues(culture);
+
+
         public static void Bind(this ILocalizationSource localize, object obj)
         {
             var props = obj
