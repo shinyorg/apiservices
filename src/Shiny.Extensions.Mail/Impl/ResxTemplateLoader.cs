3c70184f2fc19ed0e92018bc2365dac37b3fc398
Allan Ritchie <allan.ritchie@gmail.com>
2021-12-22 13:55:18 -0500

    adding a resxtemplateloader

---
+++ src/Shiny.Extensions.Mail/Impl/ResxTemplateLoader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Shiny.Extensions.Mail.Impl
+{
+    public class ResxTemplateLoader : ITemplateLoader
+    {
+        readonly ResourceManager resourceManager;
+
+
+        public ResxTemplateLoader(string baseName, Assembly assembly)
+        {
+            this.resourceManager = new ResourceManager(baseName, assembly);
+        }
+
+
+        public Task<string?> Load(string templateName, CultureInfo? culture, CancellationToken cancellationToken = default)
+        {
+            var content = this.resourceManager.GetString(templateName, culture);
+            if (content == null)
+                throw new ArgumentException($"Template {templateName} not found");
+
+            return Task.FromResult<string?>(content);
+        }
+    }
+}
