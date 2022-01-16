f1bd71a1261f98aa6d2ab06ae1e1063f3ab4d664
Allan Ritchie <allan.ritchie@gmail.com>
2022-01-16 18:32:41 -0500

    add releases to docs

--- build/Constants.cs
+++ build/Constants.cs
@@ -5,7 +5,7 @@ namespace ShinyBuild
 {
     public static class Constants
     {
-        public const string MajorMinorVersion = "1.0.0";
+        public const string MajorMinorVersion = "1.1.0";
         public const string DefaultBuildConfiguration = "Release";
     }
 }
