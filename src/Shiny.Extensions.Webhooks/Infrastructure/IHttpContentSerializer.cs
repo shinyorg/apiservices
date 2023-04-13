349e845e22ddf6ee1a8078de3416e332a13bbc10
Allan Ritchie <allan.ritchie@gmail.com>
2023-04-13 11:35:56 -0400

    Remove webhooks

--- src/Shiny.Extensions.Webhooks/Infrastructure/IHttpContentSerializer.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Net.Http;
-
-namespace Shiny.Extensions.Webhooks.Infrastructure;
-
-
-public interface IHttpContentSerializer
-{
-    HttpContent Mutate(WebHookRegistration registration, object? args);
-}
--- src/Shiny.Extensions.Webhooks/WebHookRunnerConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Shiny.Extensions.Webhooks;
-
-
-public class WebHookRunnerConfig
-{
-    public int DefaultWaitTime { get; set; } = 30;
-    public int MaxDegreeOfParallelism { get; set; } = 4;
-}
