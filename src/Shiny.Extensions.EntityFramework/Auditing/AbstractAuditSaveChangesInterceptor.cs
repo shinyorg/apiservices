42a713e14e634ba93b9ae4564765a899fb9eff89
Allan Ritchie <allan.ritchie@gmail.com>
2024-05-09 12:23:52 -0400

    Fixes

--- src/Shiny.Extensions.EntityFramework/Auditing/AbstractAuditSaveChangesInterceptor.cs
+++ src/Shiny.Extensions.EntityFramework/Auditing/AbstractAuditSaveChangesInterceptor.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Diagnostics;
+using System.Linq;
 
 namespace Shiny.Auditing;
 
--- src/Shiny.Extensions.EntityFramework/QueryableExtensions.cs
+++ src/Shiny.Extensions.EntityFramework/QueryableExtensions.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
 using Shiny.Contracts;
